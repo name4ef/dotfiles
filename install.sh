b66f197248f8b5f621e8ebfc9a1cb746cf9109e6
Efim Trivozhenko <name4ef@gmail.com>
2019-09-10 18:42:32 +0700

    changed link to dir for ncmpcpp set alsa as default for mpd

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installation of dotfiles..."
 
-files="vimrc zshrc Xresources tmux.conf xsession minttyrc ncmpcpp/config mpdconf"
+files="vimrc zshrc Xresources tmux.conf xsession minttyrc ncmpcpp mpdconf"
 
 PWD=$(pwd)
 
