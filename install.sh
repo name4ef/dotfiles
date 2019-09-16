35fcefc75fec387e43fb394b8a879b07b9887d09
Efim Trivozhenko <name4ef@gmail.com>
2019-09-16 17:33:11 +0700

    added xbindkeys to install.sh now playing track display as URL-notation :)

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installation of dotfiles..."
 
-files="vimrc zshrc Xresources tmux.conf xsession minttyrc ncmpcpp mpdconf"
+files="vimrc zshrc Xresources tmux.conf xsession minttyrc ncmpcpp mpdconf xbindkeys"
 
 PWD=$(pwd)
 
