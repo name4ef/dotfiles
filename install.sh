c6666e4ef220cb5bce6a02f6bcb41d996b7862fd
Efim Trivozhenko <name4ef@gmail.com>
2019-11-19 18:39:18 +0700

    added muttrc

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installation of dotfiles..."
 
-files="vimrc zshrc Xresources tmux.conf xsession minttyrc ncmpcpp mpdconf xbindkeys"
+files="vimrc zshrc Xresources tmux.conf xsession minttyrc ncmpcpp mpdconf xbindkeysrc muttrc"
 
 PWD=$(pwd)
 
