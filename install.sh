f95a88b187eba73046edbac2e561bcacdeab3146
Efim Trivozhenko <name4ef@gmail.com>
2021-10-01 12:24:33 +0700

    make PluginInstall after clone; normalized indent

--- install.sh
+++ install.sh
@@ -8,22 +8,22 @@ PWD=$(pwd)
 
 for file in $files
 do
-	echo ""
+    echo ""
 
-	if [ -s ~/.$file ] #exists and has a size greater than zero
-	then
-		echo "mv ~/.$file ~/.$file.back-$(date +%d%m%y_%H%M%S)"
-		mv ~/.$file ~/.$file.back-$(date +%d%m%y_%H%M%S)
-	fi
+    if [ -s ~/.$file ] #exists and has a size greater than zero
+    then
+        echo "mv ~/.$file ~/.$file.back-$(date +%d%m%y_%H%M%S)"
+        mv ~/.$file ~/.$file.back-$(date +%d%m%y_%H%M%S)
+    fi
 
-	if [ -L ~/.$file ] #exists and is a symbolic link
-	then
-		echo "rm ~/.$file"
-		rm ~/.$file
-	fi
+    if [ -L ~/.$file ] #exists and is a symbolic link
+    then
+        echo "rm ~/.$file"
+        rm ~/.$file
+    fi
 
-	echo "ln -s $PWD/$file ~/.$file"
-	ln -s $PWD/$file ~/.$file
+    echo "ln -s $PWD/$file ~/.$file"
+    ln -s $PWD/$file ~/.$file
 
 done
 
@@ -32,9 +32,8 @@ then
     echo "Installation of Vundle..."
     echo ""
 
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    #TODO some like: vim +VundleInstall
-    #TODO for vimwiki may be useful: pip3 install --user six tasklib
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim -u vimrc.tmp -c PluginInstall -c qa
 fi
 
 touch ~/.taskrc.local
