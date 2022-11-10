3a243202a104278e9e09070b71b30656718a9d73
Efim Trivozhenko <name4ef@gmail.com>
2022-11-10 09:28:38 +0700

    snownews: Binding URL to special application

--- config/snownews/browser.sh
+++ config/snownews/browser.sh
@@ -2,8 +2,13 @@
 
 URL=$1
 DOMAIN=$(echo -n $URL | cut -d "/" -f 3)
-if [[ $DOMAIN == "oper.ru" ]]; then
-    exec mpv --speed=2 "$URL" 2>&1
+if [[ $DOMAIN == "oper.ru" || $DOMAIN == "www.youtube.com" ]]; then
+    exec mpv \
+        --speed=2 \
+        --geometry="$((1920/2))x$((1080/2))+$((1920+1920/2))+$((1080/2))" \
+        "$URL" 2>&1
+elif [[ $DOMAIN == "www.opennet.ru" ]]; then
+    w3m "$URL"
 else
     exec qutebrowser --target auto "$URL"
 fi
